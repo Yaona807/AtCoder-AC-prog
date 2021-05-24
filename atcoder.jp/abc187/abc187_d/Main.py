75eacc382846a297a0852b3fd3c292b707a75b02
in708 <naoya.inoue0830@gmail.com>
2021-05-24 18:12:09 +0900

    [AC] abc187 abc187_d

---
+++ atcoder.jp/abc187/abc187_d/Main.py
@@ -0,0 +1,88 @@
+'''
+文字列取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+def readline_str(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(str, readline().rstrip().split())
+    else:
+        return readline().rstrip()
+
+
+'''
+整数取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+def readline_int(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(int, readline().rstrip().split())
+    else:
+        return int(readline().rstrip())
+
+
+'''
+整数取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+'''
+def readline_list_int(N=1):
+    import sys
+    readline = sys.stdin.readline
+    if N == 1:
+        return list(list(map(int, readline().rstrip().split())))
+    else:
+        return list(list(map(int, readline().rstrip().split()))for _ in range(N))
+
+
+'''
+文字列取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+また、split=Trueにより、
+文字列を分割した状態で格納可能
+'''
+def readline_list_str(N=1, split=False):
+    import sys
+    readline = sys.stdin.readline
+    if N == 1:
+        if split:
+            return list(readline().rstrip().split())
+        else:
+            return list(readline().rstrip())
+    else:
+        if split:
+            return list(list(readline().rstrip().split())for _ in range(N))
+
+        else:
+            return list(list(readline().rstrip())for _ in range(N))
+
+# main関数
+def main():
+    N = readline_int()
+    AB_list = readline_list_int(N=N)
+    A_list = []
+    score_list = []
+    if N == 1:
+        print(1)
+        exit()
+    for i in range(N):
+        A_list.append(AB_list[i][0])    
+        score_list.append(2*AB_list[i][0]+AB_list[i][1])
+    score_list.sort(reverse=True)
+    A_sum = sum(A_list)
+    ans = 0
+    cnt = 0
+    for i in range(N):
+        if A_sum < ans:
+            break
+        ans += score_list[i]
+        cnt += 1
+    print(cnt)
+    
+main()
