b5a124f2556e661fab2754b1ef1ff53c6ab767d5
in708 <naoya.inoue0830@gmail.com>
2021-05-30 21:21:49 +0900

    [AC] abc203 abc203_c

---
+++ atcoder.jp/abc203/abc203_c/Main.py
@@ -0,0 +1,86 @@
+'''
+文字列取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+
+
+def readline_str(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(str, readline().rstrip().split())
+    else:
+        return readline().rstrip()
+
+
+'''
+整数取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+def readline_int(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(int, readline().rstrip().split())
+    else:
+        return int(readline().rstrip())
+
+
+'''
+整数取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+'''
+def readline_list_int(N=1):
+    if N == 1:
+        return list(list(readline_int(True)))
+    else:
+        return list(list(readline_int(True))for _ in range(N))
+
+
+'''
+文字列取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+また、split=Trueにより、
+文字列を分割した状態で格納可能
+'''
+def readline_list_str(N=1, split=False):
+    if split:
+        return list(list(readline_str(True)) for _ in range(N))
+
+    else:
+        return list(list(readline_str()) for _ in range(N))
+
+
+# main関数
+def main():
+    N,K = readline_int(True)
+    A_list = list(readline_list_int() for _ in range(N))
+    A_list.sort()
+    now = 0
+    cnt = K
+    for i in range(N):
+        a = A_list[i][0]
+        b = A_list[i][1]
+        cnt -= (a - now)
+        if cnt >= 0:
+            now = a
+            cnt += b
+        elif cnt < 0:
+            ans = a + cnt
+            cnt = 0
+            break
+    if cnt != 0:
+        ans = now + cnt
+    print(ans)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
