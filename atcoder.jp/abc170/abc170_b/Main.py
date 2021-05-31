bfd18e4985cbd3f62d4eac8efd52c3c0eb7358ea
in708 <naoya.inoue0830@gmail.com>
2021-05-31 21:03:01 +0900

    [AC] abc170 abc170_b

---
+++ atcoder.jp/abc170/abc170_b/Main.py
@@ -0,0 +1,73 @@
+'''
+文字列取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+
+
+def readline_str(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(str, readline().rstrip().split())
+    else:
+        return readline().rstrip()
+
+
+'''
+整数取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+def readline_int(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(int, readline().rstrip().split())
+    else:
+        return int(readline().rstrip())
+
+
+'''
+整数取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+'''
+def readline_list_int(N=1):
+    if N == 1:
+        return list(list(readline_int(True)))
+    else:
+        return list(list(readline_int(True))for _ in range(N))
+
+
+'''
+文字列取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+また、split=Trueにより、
+文字列を分割した状態で格納可能
+'''
+def readline_list_str(N=1, split=False):
+    if split:
+        return list(list(readline_str(True)) for _ in range(N))
+
+    else:
+        return list(list(readline_str()) for _ in range(N))
+
+
+# main関数
+def main():
+    X,Y = readline_int(True)
+
+    for i in range(1,X+1):
+        if 2 * i + (X-i)*4 == Y or 4 * i + (X-i)*2 == Y:
+            print('Yes')
+            exit()
+
+    print('No')
+
+
+
+if __name__ == '__main__':
+    main()
+
