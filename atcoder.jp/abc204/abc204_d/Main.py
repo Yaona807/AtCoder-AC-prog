69f4051b3f6b18885848051916f3397dc680dd50
in708 <naoya.inoue0830@gmail.com>
2021-06-08 12:39:37 +0900

    [AC] abc204 abc204_d

---
+++ atcoder.jp/abc204/abc204_d/Main.py
@@ -0,0 +1,81 @@
+'''
+文字列取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+
+
+def readline_str(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(str, readline().rstrip().split())
+    else:
+        return readline().rstrip()
+
+
+'''
+整数取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+def readline_int(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(int, readline().rstrip().split())
+    else:
+        return int(readline().rstrip())
+
+
+'''
+整数取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+'''
+def readline_list_int(N=1):
+    if N == 1:
+        return list(list(readline_int(True)))
+    else:
+        return list(list(readline_int(True))for _ in range(N))
+
+
+'''
+文字列取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+また、split=Trueにより、
+文字列を分割した状態で格納可能
+'''
+def readline_list_str(N=1, split=False):
+    if split:
+        return list(list(readline_str(True)) for _ in range(N))
+
+    else:
+        return list(list(readline_str()) for _ in range(N))
+
+
+# main関数
+def main():
+    n = readline_int()
+    t = list(readline_int(True))
+    dp = [0 for _ in range(101001)]
+    total = 0
+    for num in t:
+        total += num
+        for j in range(1,total+1):
+            if dp[j]:
+                dp[j+num] |= dp[j]
+        dp[num] = 1
+
+    ans = total
+
+    for i in range(total+1):
+        if dp[i]:
+            ans = min(ans,max(i,(total-i)))
+
+    print(ans)    
+
+if __name__ == '__main__':
+    main()
+
