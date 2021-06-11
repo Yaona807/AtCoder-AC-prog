4f1e257413f6c40ec1751d9077cf2630daf5a238
in708 <naoya.inoue0830@gmail.com>
2021-06-11 23:28:16 +0900

    [AC] abc166 abc166_b

---
+++ atcoder.jp/abc166/abc166_b/Main.py
@@ -0,0 +1,81 @@
+'''
+文字列取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+
+
+from math import pi
+
+
+def readline_str(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(str, readline().rstrip().split())
+    else:
+        return readline().rstrip()
+
+
+'''
+整数取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+def readline_int(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(int, readline().rstrip().split())
+    else:
+        return int(readline().rstrip())
+
+
+'''
+整数取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+'''
+def readline_list_int(N=1):
+    if N == 1:
+        return list(list(readline_int(True)))
+    else:
+        return list(list(readline_int(True))for _ in range(N))
+
+
+'''
+文字列取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+また、split=Trueにより、
+文字列を分割した状態で格納可能
+'''
+def readline_list_str(N=1, split=False):
+    if split:
+        return list(list(readline_str(True)) for _ in range(N))
+
+    else:
+        return list(list(readline_str()) for _ in range(N))
+
+def f(n):
+    num_list = list(str(n))
+    g1 = sorted(num_list,reverse=True)
+    g2 = g1[::-1]
+    return int(''.join(g1)) - int(''.join(g2))
+
+# main関数
+def main():
+    N,K = readline_int(True)
+    ans = set()
+    for _ in range(K):
+        d = readline_int()
+        A = list(list(readline_int(True)))
+
+        for i in range(d):
+            ans.add(A[i])
+    print(N - len(ans))
+
+
+
+if __name__ == '__main__':
+    main()
