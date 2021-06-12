4e080a6a60b9bea1049cf972e81c5de4a05ebca0
in708 <naoya.inoue0830@gmail.com>
2021-06-13 00:40:18 +0900

    [AC] arc105 arc105_a

---
+++ atcoder.jp/arc105/arc105_a/Main.py
@@ -0,0 +1,84 @@
+'''
+文字列取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+
+
+from math import pi
+
+
+def readline_str(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(str, readline().rstrip().split())
+    else:
+        return readline().rstrip()
+
+
+'''
+整数取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+def readline_int(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(int, readline().rstrip().split())
+    else:
+        return int(readline().rstrip())
+
+
+'''
+整数取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+'''
+def readline_list_int(N=1):
+    if N == 1:
+        return list(list(readline_int(True)))
+    else:
+        return list(list(readline_int(True))for _ in range(N))
+
+
+'''
+文字列取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+また、split=Trueにより、
+文字列を分割した状態で格納可能
+'''
+def readline_list_str(N=1, split=False):
+    if split:
+        return list(list(readline_str(True)) for _ in range(N))
+
+    else:
+        return list(list(readline_str()) for _ in range(N))
+
+# main関数
+def main():
+    a,b,c,d = readline_int(True)
+    total = a+b+c+d
+    ans = 'No'
+    num_list = [a,b,c,d]
+
+    for i in range(pow(2,4)):
+        cnt = 0
+        for j in range(4):
+            if ((i>>j)&1):
+                cnt += num_list[j]
+        if total - cnt == cnt:
+            ans = 'Yes'
+            break
+
+    print(ans)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
