33c96acc0c5cbf4604e017a953cadacdfef2d21e
in708 <naoya.inoue0830@gmail.com>
2021-06-14 15:08:37 +0900

    [AC] typical90 typical90_d

---
+++ atcoder.jp/typical90/typical90_d/Main.py
@@ -0,0 +1,80 @@
+'''
+文字列取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+
+
+from math import pi
+
+
+def readline_str(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(str, readline().rstrip().split())
+    else:
+        return readline().rstrip()
+
+
+'''
+整数取得用関数
+split=Trueを引数に渡すことにより、
+mapで分割したものを戻り値とする
+'''
+def readline_int(split=False):
+    import sys
+    readline = sys.stdin.readline
+    if split:
+        return map(int, readline().rstrip().split())
+    else:
+        return int(readline().rstrip())
+
+
+'''
+整数取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+'''
+def readline_list_int(N=1):
+    if N == 1:
+        return list(list(readline_int(True)))
+    else:
+        return list(list(readline_int(True))for _ in range(N))
+
+
+'''
+文字列取得用関数(多次元配列)
+Nに数値を渡すことにより、
+N次元配列の値を取得可能
+また、split=Trueにより、
+文字列を分割した状態で格納可能
+'''
+def readline_list_str(N=1, split=False):
+    if split:
+        return list(list(readline_str(True)) for _ in range(N))
+
+    else:
+        return list(list(readline_str()) for _ in range(N))
+
+# main関数
+def main():
+    h,w = readline_int(True)
+    a = list(list(readline_int(True))for _ in range(h))
+    cnt_h = [0] * w
+    cnt_w = [0] * h
+
+    for i in range(h):
+        for j in range(w):
+            cnt_h[j] += a[i][j]
+            cnt_w[i] += a[i][j]
+    
+    for i in range(h):
+        for j in range(w):
+            print(cnt_w[i]+cnt_h[j]-a[i][j])
+
+
+        
+
+if __name__ == '__main__':
+    main()
