71150a170b30c1c036fe19cd2c16a1f14eee032b
in708 <naoya.inoue0830@gmail.com>
2021-07-31 23:00:56 +0900

    [AC] abc212 abc212_c

---
+++ atcoder.jp/abc212/abc212_c/Main.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+'''
+Function to get a string.
+--
+is_str:bool -> {
+    True: input data is 'str'.
+    False: input data is not 'str'.
+    *This function can determine the type automatically.
+}
+split:bool ->{
+    True: Split the input data.
+    False: Does not split the input data.
+    *This function can automatically determine the split.
+}
+is_list:bool ->{
+    True: Get input data in a list.
+    False: Does not get input data in a list
+}
+loop_times:int ->{
+    Used when input data is to be retrieved multiple times.
+}
+
+'''
+def inputter(is_str=False, split=False, is_list=False, loop_times=0):
+    import sys
+    readline = sys.stdin.readline
+
+    return_data_list = []
+    loop_cnt = loop_times
+
+    # Adjusting for processing
+    if loop_times == 0:
+        loop_cnt = 1
+
+    for _ in range(loop_cnt):
+        # Get input data
+        data = readline().rstrip()
+        data_split = data.split()
+
+        # Determine if input data can be split
+        if not(split) and len(data_split) > 1:
+            split = True
+
+        # Determine the type of input data
+        if not(is_str):
+            try:
+                int(data_split[0])
+            except:
+                is_str = True
+
+        # Process according to input data
+        if is_str:
+            if split:
+                if is_list:
+                    return_data = list(list(map(str, data_split)))
+                else:
+                    return_data = map(str, data_split)
+            else:
+                if is_list:
+                    return_data = list(data)
+                else:
+                    return_data = data
+        else:
+            if split:
+                if is_list:
+                    return_data = list(list(map(int, data_split)))
+                else:
+                    return_data = map(int, data_split)
+            else:
+                if is_list:
+                    return_data = list(list(int(data)))
+                else:
+                    return_data = int(data)
+
+        # Only when input data is to be acquired multiple times
+        if loop_times != 0:
+            return_data_list.append(return_data)
+
+    # Return the result
+    if loop_times == 0:
+        return return_data
+    else:
+        return return_data_list
+
+# (function) main
+def main():
+    n,m = inputter()
+    a_list = inputter(is_list=True,split=True)
+    b_list = inputter(is_list=True,split=True)
+    new_a_list = sorted(a_list)
+    new_b_list = sorted(b_list)
+
+    ans = 1010101010101010101
+    for i in new_a_list:
+        ok = 0
+        ng = m-1
+        while abs(ok-ng)>1:
+            mid = (ok+ng)//2
+
+            if i >= new_b_list[mid]:
+                ok = mid
+            else:
+                ng = mid
+        ans = min([ans,abs(i-new_b_list[ok]),abs(i-new_b_list[ng])])
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
