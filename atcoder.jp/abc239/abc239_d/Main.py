86818542451ca9ba1d2b4aeaeb00c5d3198b60e4
in708 <naoya.inoue0830@gmail.com>
2022-02-27 22:46:13 +0900

    [AC] abc239 abc239_d

---
+++ atcoder.jp/abc239/abc239_d/Main.py
@@ -0,0 +1,18 @@
+A,B,C,D = map(int,input().split())
+
+prime_list = [
+  2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
+]
+
+for i in range(A,B+1):
+  ok=True
+  for j in range(C,D+1):
+    if (i+j) in prime_list:
+      ok = False
+      break
+    
+  if ok:
+    print('Takahashi')
+    exit()
+
+print('Aoki')
